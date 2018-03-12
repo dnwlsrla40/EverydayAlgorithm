4c3289564443e3e45d490fad155d640ee9129dc8
dnwlsrla40 <dnwlsrla40@naver.com>
2018-03-13 01:12:44 +0900

    N 찍기

---
+++ acmicpc/2741.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  // 자연수 N 선언
+  int N=0;
+  int i;
+  scanf("%d", &N);
+  // N이 자연수 라면
+  if(N>0){
+    // N부터 1까지 차례로 출력
+    for(i=1; i<=N; i++){
+      printf("%d\n", i);
+    }
+  }
+  return 0;
+}
