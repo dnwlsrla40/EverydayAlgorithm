97c9388f2477c3c7eba1992b63f8b81bb0d099c6
dnwlsrla40 <dnwlsrla40@naver.com>
2018-03-13 17:17:08 +0900

    별찍기-2

---
+++ acmicpc/2439.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int N;
+  int i,j,k;
+  for(i=1; i<N; i++){
+    for(j=N; j>i; j--){
+      printf(" ");
+    }
+    printf("*");
+    printf("\n");
+  }
+  return 0;
+}
