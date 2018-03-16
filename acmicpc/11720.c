6737c62b69626741147c0fd8c12b9bf4cd68f272
woojin <dnwlsrla40@naver.com>
2018-03-16 17:30:36 +0900

    숫자의 합

---
+++ acmicpc/11720.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int N,input,sum = 0;
+	int i;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++) {
+		scanf("%1d", &input);
+		sum += input;
+	}
+	printf("%d", sum);
+	return 0;
+}
