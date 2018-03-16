15ba7b14ff261afef1c36d5e31df64130d24af67
woojin <dnwlsrla40@naver.com>
2018-03-16 17:30:54 +0900

    열 개씩 끊어 출력하기

---
+++ acmicpc/11721.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	char* input;
+	int N,i;
+	input=(char*)malloc(sizeof(char)*10);
+	scanf("%s", input);
+	for(i = 0; input[i] != NULL; i++){
+		printf("%c", input[i]);
+		if(i%10 == 9)
+			printf("\n");
+    free(input);
+    return 0;
+	}
+}
