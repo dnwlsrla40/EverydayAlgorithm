bfdff49dd95d00bf021bccd8bf1e96170375dcc2
dnwlsrla40 <dnwlsrla40@naver.com>
2018-03-17 22:18:29 +0900

    X보다 작은 수

---
+++ acmicpc/10871.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int N, X, i, input;
+	int* A;
+	scanf("%d", &N);
+	A = (int*)malloc(sizeof(int)*N);
+	scanf("%d", &X);
+	for (i = 0; i < N; i++) {
+		scanf("%d", &input);
+		A[i] = input;
+		if (A[i] < X) {
+			printf("%d ", A[i]);
+		}
+	}
+	printf("\n");
+	return 0;
+}
