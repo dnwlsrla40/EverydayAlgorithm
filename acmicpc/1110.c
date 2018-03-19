b8c825225772689c2c55d0317a487d0922ece3a4
woojin <dnwlsrla40@naver.com>
2018-03-19 14:38:58 +0900

    더하기 사이클

---
+++ acmicpc/1110.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int N, r = 0;
+	int F;
+	int count = 0;
+	int a;
+	scanf("%d", &N);
+	F = N;
+	do
+	{
+		if (N / 10 == 0) {
+			r = N;
+		}
+		else {
+			r = (N / 10) + (N % 10);
+		}
+		N = (N % 10) * 10 + (r % 10);
+		count++;
+	} while (N != F);
+	printf("%d\n", count);
+	scanf("%d", &a);
+
+	return 0;
+}
