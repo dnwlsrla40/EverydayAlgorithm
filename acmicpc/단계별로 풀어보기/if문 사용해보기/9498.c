1d5a3fbe63a2953ffb8a419adc1ad2724a746078
dnwlsrla40 <dnwlsrla40@naver.com>
2018-03-23 17:06:27 +0900

    시험 성적

---
+++ acmicpc/단계별로 풀어보기/if문 사용해보기/9498.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  int score;
+  scanf("%d", &score);
+  if(90<=score && score<=100){
+    printf("A\n");
+  }
+  else if(80<=score && score<90){
+    printf("B\n");
+  }
+  else if(70<=score && score<80){
+    printf("C\n");
+  }
+  else if(60<=score && score<70){
+    printf("D\n");
+  }
+  else {
+    printf("F\n");
+  }
+  return 0;
+}
