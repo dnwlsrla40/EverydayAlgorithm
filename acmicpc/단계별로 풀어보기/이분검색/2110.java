ee3b016aa6d4d39c69cd17bb3027674030d5d1b5
dnwlsrla40 <dnwlsrla40@naver.com>
2019-07-23 21:57:54 +0900

    2110 공유기 설치

---
+++ acmicpc/단계별로 풀어보기/이분검색/2110.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.util.Arrays;
+
+public class Java_2110 {
+    public static int binSearch(int[] Array, int left, int right, int routerCount) {
+        int result=0;
+        while(left<=right){
+            int mid = (left+right)/2;
+            int count=1;
+            int start=Array[0];
+            for(int i=0; i<Array.length; i++){
+                if(Array[i]-start >= mid){
+                    count++;
+                    start = Array[i];
+                }
+            }
+            if(count>=routerCount){
+                result= mid;
+                left = mid +1;
+            } else
+                right = mid-1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] InputArray = bf.readLine().split(" ");
+
+        int houseCount = Integer.parseInt(InputArray[0]);
+        int routerCount = Integer.parseInt(InputArray[1]);
+
+        int[] houseArray = new int[houseCount];
+        for(int i=0; i<houseCount; i++)
+            houseArray[i] = Integer.parseInt(bf.readLine());
+
+        Arrays.sort(houseArray);
+
+        int left = 1;
+        int right = houseArray[houseCount-1] - houseArray[0];
+        int result;
+        result = binSearch(houseArray, left, right, routerCount);
+        System.out.println(result);
+    }
+}
