e25083f3a160a977dbb0ea66f5cf0fca3c635708
dnwlsrla40 <dnwlsrla40@naver.com>
2019-07-27 00:40:04 +0900

    2110

--- acmicpc/단계별로 풀어보기/이분검색/2110.java
+++ acmicpc/단계별로 풀어보기/이분검색/2110.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.util.Arrays;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Java_2110 {
     public static int binSearch(int[] Array, int left, int right, int routerCount) {
